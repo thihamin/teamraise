5396956828a5a3bfbae3e60268494c6c50012bed
Thiha Min <thihakyimin@gmail.com>
2011-06-04 15:37:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamraise::Application.config.secret_token = '3ce0a629b97d3c44867d83d596d11dbb87cbf8bd91e07c1b20ed7501bd81e2db34f370225f2c65dc95df5edad1184b8a60bc58d80f7e790c421b82d041724a42'
